11476774cb8c2bacf3b9395ff28247a164ee704f
FIRST_Imad LAST_Kajdouh <2058092@talnet.nl>
2019-11-21 13:53:04 +0100

    Uitwerking van de exercise

---
+++ for-klas.php
@@ -0,0 +1,15 @@
+<?php
+$lijst = array(
+    "imad",
+    "mo",
+    "kenza",
+    "laurens",
+    "aimane",
+    "izdine",
+    "ruben",
+    "mike"
+);
+echo count ($lijst);
+for ($a = 0; $a < count($lijst); $a++) {
+    echo "\n", $lijst[$a];
+}
